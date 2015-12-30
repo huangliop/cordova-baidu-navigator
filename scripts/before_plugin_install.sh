d3bfd4f3d63d1e8a1fa034124b404131aa816dd9
huangliop <huangliop@163.com>
2015-12-30 15:34:50 +0800

    Update before_plugin_install.sh

--- scripts/before_plugin_install.sh
+++ scripts/before_plugin_install.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
+rm -rf ./plugins/cordova-baidu-navigator/src/ios/libs/BaiduNaviSDK/__MACOSX
+rm ./plugins/cordova-baidu-navigator/src/ios/libs/BaiduNaviSDK/libbaiduNaviSDK.a
 unzip ./plugins/cordova-baidu-navigator/src/ios/libs/BaiduNaviSDK/libbaiduNaviSDK.a.zip -d ./plugins/cordova-baidu-navigator/src/ios/libs/BaiduNaviSDK
